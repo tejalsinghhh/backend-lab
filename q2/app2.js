b1b9723cd65fb1ea2a7a9055dd482b9ff9e00073
tejalsingh <tejalsingh282@gmail.com>
2025-08-29 00:24:18 +0530

    Added solutions for q2 and q3

--- q2/app2.js
+++ q2/app2.js
@@ -2,4 +2,4 @@
 const lib = require('./library');
 
 console.log(lib.book.details());         // Output: Node.js Mastery by Jane Smith
-console.log(lib.utilities.greet("Pranav")); // Output: Hello, Pranav! Welcome to the library.
+console.log(lib.utilities.greet("Tejal")); // Output: Hello, Tejal! Welcome to the library.
